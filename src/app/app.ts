0f1dbb0d8c734326fc7fa01c5e1bf2c018c8055d
notes22081402 <notes22a08mr14m02ot@gmail.com>
2025-06-14 17:24:35 +0530

    kalpana-add hom page

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,7 +5,7 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  //templateUrl: './app.html',
+  templateUrl: './app.html',
   template: `<router-outlet></router-outlet>`,
   styleUrl: './app.css'
 })
